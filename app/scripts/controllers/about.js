b19c34e2b269ef94e10eca2f64cb7e38a60c53d5
Alexandru Constantin <alex@aconstantin-ws.vendio.ro>
2015-01-28 13:16:04 +0200

    Boilerplate

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name muzpuzApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the muzpuzApp
+ */
+angular.module('muzpuzApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name muzpuzApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the muzpuzApp
+ */
+angular.module('muzpuzApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
